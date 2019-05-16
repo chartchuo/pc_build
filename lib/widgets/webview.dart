2990d30bab4aa599b043d3664f66cb044f5bf1bd
chartchuo <chartchuo@gmail.com>
2019-05-16 16:05:01 +0700

    migrade to AndroidX and add webview_flutter

--- lib/widgets/webview.dart
+++ lib/widgets/webview.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-// class WebviewPage extends StatelessWidget {
-//   final String url;
-//   final String title;
+class WebviewPage extends StatelessWidget {
+  final String url;
+  final String title;
 
-//   WebviewPage({Key key, this.url, this.title}) : super(key: key);
+  WebviewPage({Key key, this.url, this.title}) : super(key: key);
 
-//   @override
-//   Widget build(BuildContext context) {
-// return WebviewScaffold(
-//   url: url,
-//   appBar: AppBar(
-//     title: Text(title),
-//   ),
-// );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: url,
+    );
+  }
+}
